349299490e7de84c2ef6a17cf0bedef25ca3013c
myo-i <Imiyo0521@gmail.com>
2024-04-04 00:27:10 +0900

    implement update

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { ItemStatus } from './item-status.enum';
 import { Item } from './items.model';
@@ -34,5 +34,10 @@ export class ItemsController {
             status: ItemStatus.ON_SALE,
         }
         return this.itemsService.create(item);
+    }
+
+    @Patch(':id')
+    updateStatus(@Param('id') id : string): Item{
+        return this.itemsService.updateStatus(id)
     }
 }
