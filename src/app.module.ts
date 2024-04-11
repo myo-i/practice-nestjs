dcf009f4473a54ca0ec9a2ab8853977aed330e39
myo-i <Imiyo0521@gmail.com>
2024-04-12 03:37:05 +0900

    define DataSource for typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from 'ormconfig';
 
 
 @Module({
-  imports: [
-    ItemsModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'postgres',
-      autoLoadEntities: true,
-    })
-  ],
+  imports: [ItemsModule,TypeOrmModule.forRoot(dataSourceOptions)],
   controllers: [],
   providers: [],
 })
